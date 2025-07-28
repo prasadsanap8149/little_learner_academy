552dc91e2ce26ee9087ba08206e0a6cd62d717e2
Prasad Sanap <85665375+prasadsanap8149@users.noreply.github.com>
2025-07-28 20:46:53 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
 import 'services/game_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const LittleLearnersApp());
 }
 
