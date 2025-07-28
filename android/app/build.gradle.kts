0f61a324fda4ff1e7110e229ec95ece2fe0f414b
prasadsanap8149 <prasadsanap8149@gmail.com>
2025-07-28 20:36:41 +0530

    Gradle Error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id ("com.google.gms.google-services")
 }
 
 android {
@@ -42,3 +43,16 @@ android {
 flutter {
     source = "../.."
 }
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+
+    // Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
+    implementation 'com.google.android.gms:play-services-ads:24.4.0'
+}
+
+apply plugin: 'com.google.gms.google-services'
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,7 +3,14 @@ allprojects {
         google()
         mavenCentral()
     }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1") // or your existing Android Gradle Plugin version
+        classpath("com.google.gms:google-services:4.4.3") // ✅ FIXED
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
 }
+
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
